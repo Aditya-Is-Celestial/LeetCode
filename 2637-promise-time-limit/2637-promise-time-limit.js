adffacf032b354152122256a8150446fdddc9988
Aditya-Is-Celestial <131970831+Aditya-Is-Celestial@users.noreply.github.com>
2023-08-23 13:22:36 +0530

    Time: 65 ms (35.01%), Space: 41.6 MB (86.42%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+         const originalPromise=fn(...args);
+           const rejectPromise=new Promise((resolve,reject)=>{setTimeout(()=>{
+               reject("Time Limit Exceeded")
+           },t);
+           });
+          return Promise.race([originalPromise,rejectPromise]); 
+    }
+        };
+
+    
+    
+
